ceaa55fe20354b4005b2afbbd5dc9857a77d1d52
小林怜央 <@DESKTOP-NG58O99>
2017-03-20 00:12:49 +0900

    「ウィンドウを作る」実装

--- StopWatch/main.cpp
+++ StopWatch/main.cpp
@@ -1,11 +1,32 @@
-#include<windows.h>
+
 #include "stdafx.h"
 
 int WINAPI WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
 	PSTR lpCmdLine,
-	int nCmdShow) {
+	int nCmdShow) 
+{
+	HWND window_handle = CreateWindow(
+		TEXT("STATIC"),    //ウィンドウクラス
+		TEXT("ウィンドウだよ"), //ウィンドウ名
+		WS_CAPTION,       //ウィンドウのスタイル(CAPTIONは、タイトルバーを持つやつ)
+		500,              //初期位置(x軸)
+		100,              //初期位置(y軸)
+		100,              //ウィンドウの幅(x軸)
+		1000,              //ウィンドウの幅(y軸)
+		NULL,             //親ウィンドウのハンドル
+		NULL,             //メニューのハンドル
+		hInstance,         //インスタンスハンドル(メインの引数で受け取る、プログラムのハンドル)
+		NULL              //メッセージに渡されるパラメータ
+	);
+
+	if (window_handle == NULL)
+	{
+		return 0; //エラーチェック。ハンドルがNULLだったらエラーなので、プログラム終了。
+	}
+
+	ShowWindow(window_handle, SW_SHOW);
 	MessageBox(NULL, TEXT("Kitty on your lap"),
 		TEXT("メッセージボックス"), MB_OK);
 	return 0;
